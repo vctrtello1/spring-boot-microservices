923029fe7e4ceb5cea1fb524589fdf82c573e61d
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-27 16:43:33 -0600

    CurrencyExchangeRepsository

--- currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeServiceController.java
+++ currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeServiceController.java
@@ -1,7 +1,5 @@
 package com.victortello.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeServiceController {
 
     @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
+    @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null){
+            throw new RuntimeException("Unable to find for " + from + " to " + to);
+        }
 
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
-    }    
+    }
 }

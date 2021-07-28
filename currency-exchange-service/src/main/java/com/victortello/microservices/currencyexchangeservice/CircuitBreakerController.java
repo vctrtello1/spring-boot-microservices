1ffc46a3634eb8940cdca613d3852ebd5e955d67
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-28 16:03:14 -0600

    resiliance4j

---
+++ currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,30 @@
+package com.victortello.microservices.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "harcodedResponse")
+    public String sampleApi(){
+        logger.info("Sample api call received");
+
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("localhost:8080/some-dummy",
+        String.class);
+        return forEntity.getBody();
+    }
+
+    public String harcodedResponse(Exception ex) {
+        return "harcoded-Response";
+    }
+}

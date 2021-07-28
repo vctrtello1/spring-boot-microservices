329b89c2f03b5e9843c811f39c798b2510ca810f
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-28 16:31:42 -0600

    circuit breaker options

--- currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+//import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+
+//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+//import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,7 +19,10 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "harcodedResponse")
+    //@CircuitBreaker(name="api", fallbackMethod = "harcodedResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "harcodedResponse")
+    //@RateLimiter(name = "default")
+    //@Bulkhead(name = "sample-api")
     public String sampleApi(){
         logger.info("Sample api call received");
 

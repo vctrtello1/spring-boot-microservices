83fd373141e139e02f3c49c17ede869c59576fb3
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-27 20:20:36 -0600

    CurrencyExchange proxy

--- currency-conversion-service/src/main/java/com/victortello/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/victortello/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.victortello.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,10 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
+        @Autowired
+        private CurrencyExchangeProxy proxy;
+
         @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
         public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
                         @PathVariable BigDecimal quantity) {
@@ -24,6 +29,23 @@ public class CurrencyConversionController {
                                 uriVariables);
 
                 CurrencyConversion currencyConversion = responseEntity.getBody();
+
+                return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                                currencyConversion.getConversionMultiple(),
+                                quantity.multiply(currencyConversion.getConversionMultiple()),
+                                currencyConversion.getEnvironment());
+        }
+
+        @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+        public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+                        @PathVariable BigDecimal quantity) {
+
+                HashMap<String, String> uriVariables = new HashMap<>();
+                uriVariables.put("from", from);
+                uriVariables.put("to", to);
+                
+                CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+                
 
                 return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
                                 currencyConversion.getConversionMultiple(),

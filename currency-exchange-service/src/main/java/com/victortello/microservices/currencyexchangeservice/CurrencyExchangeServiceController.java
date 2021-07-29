452a600cd81db5bae531bd7c6b3db594d565547d
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-07-28 19:53:40 -0600

    zipkin dependencies

--- currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeServiceController.java
+++ currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeServiceController.java
@@ -1,6 +1,8 @@
 package com.victortello.microservices.currencyexchangeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeServiceController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeServiceController.class);
 
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
@@ -17,6 +21,9 @@ public class CurrencyExchangeServiceController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 
         if(currencyExchange == null){

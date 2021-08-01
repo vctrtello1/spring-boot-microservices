f0e1464e091f6588754453dc4d9982f40e235fc3
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-01 17:24:02 -0600

    version 12

--- currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/victortello/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -32,7 +32,7 @@ public class CurrencyExchangeController {
 
         String port = environment.getProperty("local.server.port");
         String host = environment.getProperty("HOSTNAME");
-        String version = "v11";
+        String version = "v12";
 
         currencyExchange.setEnvironment(port + " " + version + " " + host);
         return currencyExchange;
